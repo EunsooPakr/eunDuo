ae3549047d0df7fcc3decb93c92feef97980f2d9
DESKTOP-UJ2U04N\박은수 <elfflqjem3366@naver.com>
2024-12-10 15:23:43 +0900

    작성일:2024-12-10 작성시간:15:23 작성자:은튜 메모: - 프로젝트 생성

---
+++ eunDuo/src/test/java/eunduo/eunduo/EunDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package eunduo.eunduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EunDuoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
