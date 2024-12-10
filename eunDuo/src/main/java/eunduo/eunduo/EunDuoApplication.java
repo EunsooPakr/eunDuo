9fee9bf52321c1e37b7aac0c4ec1bf409cd472a5
DESKTOP-UJ2U04N\박은수 <elfflqjem3366@naver.com>
2024-12-10 15:46:25 +0900

    no message

---
+++ eunDuo/src/main/java/eunduo/eunduo/EunDuoApplication.java
@@ -0,0 +1,13 @@
+package eunduo.eunduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EunDuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EunDuoApplication.class, args);
+	}
+
+}
